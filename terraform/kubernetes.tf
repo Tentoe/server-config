7d86cd686ccbaf9a984edec8ca6c6a5539448162
Keven Kurz <keven.kurz@gmail.com>
2020-05-15 08:18:18 +0200

    update playbook added kubeworker1

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -20,6 +20,12 @@ resource "openstack_compute_instance_v2" "kubemaster0" {
   }
 }
 
+resource "openstack_dns_recordset_v2" "kubemaster0" {
+  zone_id = openstack_dns_zone_v2.default.id
+  name    = "${openstack_compute_instance_v2.kubemaster0.name}.${openstack_dns_zone_v2.default.name}"
+  type    = "A"
+  records = [openstack_compute_instance_v2.kubemaster0.access_ip_v4]
+}
 
 resource "openstack_compute_instance_v2" "kubeworker0" {
 
@@ -43,16 +49,38 @@ resource "openstack_compute_instance_v2" "kubeworker0" {
   }
 }
 
-resource "openstack_dns_recordset_v2" "kubemaster0" {
-  zone_id = openstack_dns_zone_v2.default.id
-  name    = "${openstack_compute_instance_v2.kubemaster0.name}.${openstack_dns_zone_v2.default.name}"
-  type    = "A"
-  records = [openstack_compute_instance_v2.kubemaster0.access_ip_v4]
-}
-
 resource "openstack_dns_recordset_v2" "kubeworker0" {
   zone_id = openstack_dns_zone_v2.default.id
   name    = "${openstack_compute_instance_v2.kubeworker0.name}.${openstack_dns_zone_v2.default.name}"
   type    = "A"
   records = [openstack_compute_instance_v2.kubeworker0.access_ip_v4]
+}
+
+resource "openstack_compute_instance_v2" "kubeworker1" {
+
+  name      = "kubeworker1"
+  flavor_id = openstack_compute_flavor_v2.medium.id
+  key_pair  = openstack_compute_keypair_v2.default.id
+  security_groups = [
+    openstack_networking_secgroup_v2.all.name
+  ]
+
+  network {
+    uuid = openstack_networking_network_v2.wireguard.id
+  }
+
+  block_device {
+    uuid                  = openstack_images_image_v2.ubuntu_bionic.id
+    source_type           = "image"
+    volume_size           = 50
+    destination_type      = "volume"
+    delete_on_termination = true
+  }
+}
+
+resource "openstack_dns_recordset_v2" "kubeworker1" {
+  zone_id = openstack_dns_zone_v2.default.id
+  name    = "${openstack_compute_instance_v2.kubeworker1.name}.${openstack_dns_zone_v2.default.name}"
+  type    = "A"
+  records = [openstack_compute_instance_v2.kubeworker1.access_ip_v4]
 }
